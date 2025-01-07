d1da5234230e536e5609b0959a37f4f77f56bdaa
Woohyun Jo <bluewing1004@gmail.com>
2025-01-07 11:44:50 +0900

    VC의 TabelView 레이아웃

---
+++ SecondApp/Ten/TenViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TenViewController.swift
+//  SecondApp
+//
+//  Created by 조우현 on 1/7/25.
+//
+
+import UIKit
+
+// 왜 여기서는 CellForRowAt이 안나올까?
+// 테이블뷰 아웃렛
+// 테이블뷰를 만들 때 필요한 것들이 있는데 -> 이거 수동으로 다 해줘야ㅕ 함
+
+class TenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

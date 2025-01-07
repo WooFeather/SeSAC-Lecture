bb7ea29f5b6e337333cce77deac305774a78ca69
Woohyun Jo <bluewing1004@gmail.com>
2025-01-07 12:20:18 +0900

    CollectionView 세팅

---
+++ SecondApp/Ten/TenCollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TenCollectionViewController.swift
+//  SecondApp
+//
+//  Created by 조우현 on 1/7/25.
+//
+
+import UIKit
+
+class TenCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}

039b6b3cea2f8aede12bf65c5750378bc8be9a5f
Woohyun Jo <bluewing1004@gmail.com>
2025-01-16 14:49:38 +0900

    [17th] 싱글톤패턴

--- FourthWeek/FourthWeek/BookViewController.swift
+++ FourthWeek/FourthWeek/BookViewController.swift
@@ -12,9 +12,6 @@ class BookViewController: UIViewController {
     // 원래의 view와 구분짓기 위한 네이밍
     var mainView = BookView()
     
-    // UIView를 내가 만든 커스텀 View로 대체하도록 만들어줌
-    // 이친구는 super를 쓰지 않음
-    // 그 이유는, super를 통해 view의 설정을 다시 가져오면 내가 만든 커스텀View가 안보일수도 있기 때문
     override func loadView() {
         view = mainView
     }
@@ -22,7 +19,8 @@ class BookViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 이제 collectionView가 mainView에 있기 떄문에 이렇게 수정
+        UserDefaultsManager.shared.age = 50
         
+        print(UserDefaultsManager.shared.age)
     }
 }

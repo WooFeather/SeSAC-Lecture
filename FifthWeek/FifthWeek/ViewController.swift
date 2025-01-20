a6a745576dc2ec4de3f90e6f40feb148c8492b2f
Woohyun Jo <bluewing1004@gmail.com>
2025-01-21 07:58:53 +0900

    [19th] GCD 연습

--- FifthWeek/FifthWeek/ViewController.swift
+++ FifthWeek/FifthWeek/ViewController.swift
@@ -46,12 +46,164 @@ class ViewController: UIViewController {
         
         configureView()
         
-        example2()
+        
+        example3()
+//        example2()
 //        example()
 //        concurrentAsync()
 //        concurrentSync()
 //        serialAsync()
 //        serialSync()
+    }
+    
+    func example3() {
+        print("🔵START", terminator: " ")
+        
+        // DeadLock
+//        DispatchQueue.main.sync {
+//            for item in 1...10 {
+//                print(item, terminator: " ")
+//            }
+//        }
+        
+        // serial async => 맨 마지막에 실행될듯 🅾️
+        DispatchQueue.main.async {
+            for item in 11...20 {
+                print(item, terminator: " ")
+            }
+        }
+        
+        
+        // concurrent sync => 출력순서는 serial sync와 같을것이고, main이 아무일도 안하기 때문에 잘 사용하지 않음 🅾️
+        DispatchQueue.global().sync {
+            for item in 21...30 {
+                print(item, terminator: " ")
+            }
+        }
+        
+        // 다른 스레드로 보낸 상태에서 concurrent sync => 순서가 섞여서 나올듯 🅾️
+        // 이 숫자뭉치는 섞여서 나오지만(어느 시점에든 들어감), 31~40 순서는 유지됨
+        DispatchQueue.global().async {
+            DispatchQueue.global().sync {
+                for item in 31...40 {
+                    print(item, terminator: " ")
+                }
+            }
+        }
+        
+        // 이중 concurrent async => 숫자뭉치, 숫자뭉치 내 전부 섞여서 나올듯 ❎
+        // 숫자뭉치는 다른 숫자들 사이에 섞여서 나오지만, 41~50 사이의 숫자는 순서대로 나옴
+        DispatchQueue.global().async {
+            DispatchQueue.global().async {
+                for item in 41...50 {
+                    print(item, terminator: " ")
+                }
+            }
+        }
+        
+        // concurrent async => 숫자뭉치는 다른 숫자들 사이에 섞이고, 순서는 지켜질것 🅾️
+        DispatchQueue.global().async {
+            for item in 51...60 {
+                print(item, terminator: " ")
+            }
+        }
+        
+        // for문 두개를 감싼 concurrent async => 숫자뭉치는 다른 숫자들 사이에 섞이고, 순서(61~80)는 지켜질것 🅾️
+        DispatchQueue.global().async {
+            for item in 61...70 {
+                print(item, terminator: " ")
+            }
+            
+            for item in 71...80 {
+                print(item, terminator: " ")
+            }
+        }
+        
+        // concurrent async 안의 serial async => 91-100 숫자뭉치는 섞이지 않고 그대로 출력, 그러나 두 숫자뭉치 모두 그 위치 자체는 섞임 ❎
+        // 81-90은 순서는 지켜지고 위치가 섞인것은 맞지만, 91-100 뭉치는 순서는 유지, 그리고 위치는 항상 맨 마지막에 출력임
+        DispatchQueue.global().async {
+            for item in 81...90 {
+                print(item, terminator: " ")
+            }
+            
+            DispatchQueue.main.async {
+                for item in 91...100 {
+                    print(item, terminator: " ")
+                }
+            }
+        }
+        
+        // 이중 serial async => 이 두 숫자뭉치는 맨 마지막에 출력될것이고, 그중에서 101-110이 더 마지막에 출력될듯 ❓
+        // 일단 101~110이 맨 마지막에 출력되는건 맞지만, 111~120이 출력되고 그 다음에 91~100이 먼저 출력되고 101~110이 맨 마지막
+        // 111~120과 91~100의 순서는 어떻게 결정되지?
+        // 실행되면 이 두개의 반복문은 즉시 맨 뒤로감 -> 91-100반복문은 일단 다른 스레드로 갔다가 맨 뒤로 옴 -> 그중 101-110반목문은 맨 뒤로 감
+        // 그래서 111-120 -> 91-100 -> 101-110인듯
+        DispatchQueue.main.async {
+            DispatchQueue.main.async {
+                for item in 101...110 {
+                    print(item, terminator: " ")
+                }
+            }
+            
+            for item in 111...120 {
+                print(item, terminator: " ")
+            }
+        }
+        
+        // attributes를 지정해주지 않는다면 기본적으로 Serial, qos도 지정할 수 있음
+        let customQueue = DispatchQueue(label: "serial async")
+        
+        // custom serial async 안의 serial sync => 맨 마지막에 실행 🅾️
+        // 일단 커스텀 큐로 보내지고, 121-130은 다른 작업이 끝날때까지 기다릴것
+        customQueue.async {
+            DispatchQueue.main.sync {
+                for item in 121...130 {
+                    print(item, terminator: " ")
+                }
+            }
+        }
+        
+        let customQueue2 = DispatchQueue(label: "concurrent async", attributes: .concurrent)
+        
+        // custom concurrent async 안의 serial sync => 얘도 아마 맨 마지막에 실행..? 🅾️
+        // 역시 일단 커스텀 큐로 보내지고, 131-140은 다른 작업이 끝날때까지 기다릴것
+        customQueue2.async {
+            DispatchQueue.main.sync {
+                for item in 131...140 {
+                    print(item, terminator: " ")
+                }
+            }
+        }
+        
+        // custom serial async안의 concurrent async => 151-160은 순서대로, 161-170은 순서는 지키되 위치는 무작위 ❎
+        // 151-170 전부 맨 마지막에 실행되고 순서도 지켜짐...
+        customQueue.async {
+            for item in 151...160 {
+                print(item, terminator: " ")
+            }
+            
+            DispatchQueue.global().async {
+                for item in 161...170 {
+                    print(item, terminator: " ")
+                }
+            }
+        }
+        
+        // custom concurrent async 안의 concurrent async => 두 숫자뭉치의 순서는 유지, 위치는 무작위 ❎
+        // 둘 다 순서 지켜서 차례대로
+        customQueue2.async {
+            for item in 171...180 {
+                print(item, terminator: " ")
+            }
+            
+            DispatchQueue.global().async {
+                for item in 181...190 {
+                    print(item, terminator: " ")
+                }
+            }
+        }
+        
+        print("END🔴", terminator: " ")
     }
     
     // DispatchGroup
@@ -209,6 +361,8 @@ class ViewController: UIViewController {
         checkButton.addTarget(self, action: #selector(checkButtonTapped), for: .touchUpInside)
     }
     
+    
+    // DispatchGroup
     @objc
     func checkButtonTapped() {
         
